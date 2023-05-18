c6df0270958f02639bd788e3d66561f2e006918e
Jerry.jiao <jerry201382@gmail.com>
2023-05-18 10:23:59 +0800

    feat: first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
